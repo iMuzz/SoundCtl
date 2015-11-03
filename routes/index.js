f23ffedafe570644bc1a70c837258f08893c8d8a
Faraaz Nishtar <faraaznishtar@Faraazs-MBP.hackerdojo.com>
2015-11-03 00:34:11 -0800

    create route with player route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/player', function(req, res, next) {
+  res.render('player', { title: 'Express' });
+});
+
+
 module.exports = router;
