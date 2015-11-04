2aca2467b432021abd6bfdd48a71d7fa3566aeda
Faraaz Nishtar <faraaznishtar@Faraazs-MBP.hackerdojo.com>
2015-11-04 02:51:24 -0800

    getting mountpoint from krad api through the service

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,14 @@
 (function(){
 	var soundCtl = angular.module('soundCtl', []);
 
-	soundCtl.controller('webPlayerController', ['$scope', function($scope){
+	soundCtl.controller('webPlayerController', ['$scope','streamOutput', function($scope, streamOutput){
 		$scope.player = {
 			isPlaying: false
 		};
+
+		var outPutUrl = streamOutput.getOutputURL().then(function(res){
+			console.log('Answer:', res);
+		});
 	}]);
 
 	soundCtl.directive('audioPlayer', function(){
@@ -24,4 +28,53 @@
 			}
 		}
 	});
+
+	soundCtl.factory("streamOutput", ['$http','$q', function($http, $q){
+		
+		var streamOutput = {};
+
+		streamOutput.getOutputURL = function(){
+			var mountPoints = [];
+
+			// API call  and get studio paths
+			var promise = $http.get('http://kradradio/studio').then(function(res){
+				var promises = [];
+				
+				for(var i = 0; i < res.data.length; i++) {
+					console.log('Requesting path ' + i);
+					promises.push($http.get('http://kradradio'+res.data[i]).success(searchForOutput));
+				}
+				
+				return $q.all(promises).then(function(){
+					// return mountPoints;
+					return generateStreamUrls(mountPoints);
+				})
+			});
+			
+			function generateStreamUrls(mountPoints){
+				var urls = [];
+				for (var i = 0; i < mountPoints.length; i++) {
+					urls.push("http://kradradio/" + mountPoints[i]);
+				}
+				return urls;
+			}
+
+			function searchForOutput(studioPath) {
+				console.log('searching for output..');
+				if (studioPath['output'] !== undefined ) {
+					if (studioPath['output']['transmission'] !== undefined) {
+						console.log("output found:", studioPath['output']['transmission']['mount']);
+						mountPoints.push(studioPath['output']['transmission']['mount']);
+					};
+				};
+			}
+
+			return promise;
+		}
+
+		
+		return streamOutput;
+	}]);
+
+
 })();
