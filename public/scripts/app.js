8eb3366dbd873df234f86d61f209d7c94e94e5b5
Faraaz Nishtar <faraaznishtar@Faraazs-MBP.hackerdojo.com>
2015-11-03 20:38:11 -0800

    player styling complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,27 @@
-console.log("hey there!");
+(function(){
+	var soundCtl = angular.module('soundCtl', []);
+
+	soundCtl.controller('webPlayerController', ['$scope', function($scope){
+		$scope.player = {
+			isPlaying: false
+		};
+	}]);
+
+	soundCtl.directive('audioPlayer', function(){
+		return {
+			restrict: 'E',
+			templateUrl: "/templates/music-player",
+			controller: function($scope, $element){
+				$scope.toggleStream = function(){
+					var audioTag = $element.find("audio")[0];
+					if ($scope.player.isPlaying) {
+						audioTag.pause();
+					} else {
+						audioTag.play();
+					}
+					$scope.player.isPlaying = !$scope.player.isPlaying;
+				};
+			}
+		}
+	});
+})();
