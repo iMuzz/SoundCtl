aef357d10aa1251c32ab6fcd44ce2a86825cf052
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-12-01 22:33:55 -0800

    update auth0Client.js to newest version

--- modules/auth0Client.js
+++ modules/auth0Client.js
@@ -1,17 +1,20 @@
-var request = require('request');
+var request = require('request-promise');
 
-auth0Client = {
+// Generic header Options for Auth0 Requests
+var options = {
+	url: 'https://soundctl.auth0.com/api/v2/users/google-oauth2%7C',
+	headers: {
+		'User-Agent': 'request',
+		'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiI2aVRqUjJIa2hBTFBQb0RmSThmZU4wWjFmbFhKOVlKTyIsInNjb3BlcyI6eyJ1c2VycyI6eyJhY3Rpb25zIjpbInVwZGF0ZSJdfX0sImlhdCI6MTQ0OTAxODExMiwianRpIjoiNzI5ZDI0NDI2NDgyYTRkNmZiNDU4MWVjN2Y3YWU1YzcifQ.o6jQBw6lb4-phi3UNeFDk61sdVJSG8kgJyNS8kwGA50'
+	}
+}
+
+var auth0Client = {
 	
 	// @param userID (Auth0 user_id of a single user)
 	// @param AuthToken (Auth0 refresh token)
 	getUser: function(userID){
-		var options = {
-		  url: 'https://soundctl.auth0.com/api/v2/users' + userID,
-		  headers: {
-		    'User-Agent': 'request',
-		    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiI2MlRObWZOWTI0VnRXTDBkd2RNMW84aUdkSHd2RGNlUSIsInNjb3BlcyI6eyJ1c2VycyI6eyJhY3Rpb25zIjpbInVwZGF0ZSIsInJlYWQiXX19LCJpYXQiOjE0NDgxOTA4MjcsImp0aSI6ImI1MmM2ZTNhMGMzNjk3ZWZhMDM3MGM0OTEzOTBlMTY1In0.jd4HH5RyYNZ6qA4RNdskRmn4ReLYnQmgnPx-fSaBx2k'
-		  }
-		};
+		options.url = options.url + userID;
 
 		request(options, function(error, response, body){
 			//TODO: Add error check at this point
@@ -21,28 +24,21 @@ auth0Client = {
 		});
 	},
 
-	updateAppData: function(userID, payLoad){
-		console.log('updateAppData called!');
-		var options = {
-		  method: 'PATCH',
-		  url: 'https://soundctl.auth0.com/api/v2/users/google-oauth2%7C107749376288480166720',
-		  headers: {
-		    'User-Agent': 'request',
-		    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiI2MlRObWZOWTI0VnRXTDBkd2RNMW84aUdkSHd2RGNlUSIsInNjb3BlcyI6eyJ1c2VycyI6eyJhY3Rpb25zIjpbInVwZGF0ZSIsInJlYWQiXX19LCJpYXQiOjE0NDgxOTA4MjcsImp0aSI6ImI1MmM2ZTNhMGMzNjk3ZWZhMDM3MGM0OTEzOTBlMTY1In0.jd4HH5RyYNZ6qA4RNdskRmn4ReLYnQmgnPx-fSaBx2k',
-		    'Content-Type': 'application/json'
-		  },
-		  json: {app_metadata: JSON.parse(payLoad)}
-		};
+	// 
+	updateAppMetaData: function(userID, payLoad){
+		// console.log('updateAppData called!');
+		options['method'] = 'PATCH';
+		options['url'] = options.url + userID;
+		options['headers']['Content-Type'] = 'application/json';
+		options['json'] = {app_metadata: payLoad}
 
-		request(options, function(error, response, body){
-			if(error) {
-				console.log(body);
-			}
-			//TODO: Add error check at this point
-			if(!error && response.statusCode == 200) {
-			  console.log("Response from getUser", body);
-			}
-		});
+		return request(options)
+			.then(function(response){
+				return response;
+			})
+			.catch(function(err){
+				console.log(err);
+			})
 
 	}
 }
