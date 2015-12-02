6391bf0699076d8fb5e08e824d3c9dc27f5bd21a
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-12-01 22:15:36 -0800

    change callback url

--- public/scripts/auth0.js
+++ public/scripts/auth0.js
@@ -3,7 +3,7 @@ var lock = new Auth0Lock('da8oL0bZSljscKr94Oq11W7P7AiTvb4L', 'soundctl.auth0.com
 
 function signin() {
   lock.show({
-      callbackURL: 'http://localhost:3000/callback'
+      callbackURL: 'http://alpha.soundctl.com/callback'
     , responseType: 'code'
     , authParams: {
       scope: 'openid name email picture'
