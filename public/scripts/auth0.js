527a39cdf8cb8ab3dc2d6e306c7f62d3296deda2
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-12-01 22:22:21 -0800

    fix spacing

--- public/scripts/auth0.js
+++ public/scripts/auth0.js
@@ -3,9 +3,9 @@ var lock = new Auth0Lock('da8oL0bZSljscKr94Oq11W7P7AiTvb4L', 'soundctl.auth0.com
 
 function signin() {
   lock.show({
-      callbackURL: 'http://alpha.soundctl.com/callback'
-    , responseType: 'code'
-    , authParams: {
+      callbackURL: 'http://alpha.soundctl.com/callback',
+      responseType: 'code', 
+      authParams: {
       scope: 'openid name email picture'
     }
   });
