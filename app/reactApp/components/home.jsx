917492410ffb2ea54c23d25ce2edb27c681a1764
Faraaz Nishtar <faraaznishtar@gmail.com>
2016-03-04 14:57:16 -0800

    countdown complete

--- app/reactApp/components/home.jsx
+++ app/reactApp/components/home.jsx
@@ -58,6 +58,7 @@ class HeroSection extends React.Component {
 					<div className="bg-hero bg-hero-1"></div>
 					<div className="hero-content">
 						<div className="slogan animated fadeInDown delay-half-sec"> Audio mixing, from the future </div>
+						<Countdown className="animated fadeInDown delay-1s" endTime='Thu Mar 17 2016 00:56:22 GMT-0700'/>
 						{/*
 						<form id="mc-embedded-subscribe-form" action="//SoundCtl.us12.list-manage.com/subscribe/post?u=be4c8e8b746c8bbb27b92210e&amp;id=7228dc16ba" name="mc-embedded-subscribe-form" method="post" target="_blank">
 							<input placeholder="Enter email address" type="email" name="EMAIL" required/>
@@ -153,6 +154,7 @@ export class PricingPage extends React.Component {
 					</div>
 					<h2> 100% money back guarantee for the first 30 days on any paid plan. </h2>
 				</div>
+				<Countdown />
 				<Footer />
 			</div>
 		);
@@ -241,7 +243,6 @@ export class Footer extends React.Component {
 	}
 }
 
-
 export class InfoSection extends React.Component {
 	constructor(props){
 		super(props);
@@ -273,5 +274,70 @@ export class InfoSection extends React.Component {
 				</div>
 			</div>
 		)
+	}
+}
+
+class Countdown extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.getTimeRemaining = this.getTimeRemaining.bind(this);
+		this.updateRemaingTime = this.updateRemaingTime.bind(this);
+		this.componentDidMount = this.componentDidMount.bind(this);
+		this.componentWillUnmount = this.componentWillUnmount.bind(this);
+
+		this.state = this.getTimeRemaining();
+	}
+
+	updateRemaingTime(){
+		this.setState(this.getTimeRemaining());
+	}
+
+	getTimeRemaining(){
+		var t = Date.parse(this.props.endTime) - Date.parse(new Date());
+		var seconds = Math.floor((t / 1000) % 60);
+		var minutes = Math.floor((t / 1000 / 60) % 60);
+		var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+		var days = Math.floor(t / (1000 * 60 * 60 * 24));
+		var answer = {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+		return answer;
+	}
+
+	componentDidMount(){
+		console.log("Component mounting...");
+		this.interval = setInterval(this.updateRemaingTime, 1000);
+	}
+
+	componentWillUnmount(){
+		console.log("Countdown unmounting...");
+		clearInterval(this.interval);
+	}
+
+	render() {
+		return  ( 
+			<div className="countdown">
+				<div className="time-wrap">
+					<div>
+						<div className="time" > {this.state.days}   </div>
+						<div className="time" > {this.state.hours} </div>
+						<div className="time" > {this.state.minutes} </div>
+						<div className="time" > {this.state.seconds} </div>
+					</div>
+					<div className="unit-wrap">
+						<div className="unit"> Days </div>
+						<div className="unit"> Hours </div>
+						<div className="unit"> Minutes </div>
+						<div className="unit"> Seconds </div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
