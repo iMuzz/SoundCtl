6a99559d17ba6c018b2a112e9369d922fd163f6c
Faraaz Nishtar <faraaznishtar@gmail.com>
2016-03-04 18:49:22 -0800

    Footer re-organized and styled for mobile

--- app/reactApp/components/home.jsx
+++ app/reactApp/components/home.jsx
@@ -255,41 +255,30 @@ export class Footer extends React.Component {
 	render() {
 		return (
 			<div className="footer-wrap">
-				<table className="uk-table">
-					<tbody>
-						<thead>
-							<tr>
-								<th> Company</th>
-								<th> Product</th>
-								<th> Support</th>
-								<th> Connect</th>
-							</tr>
-						</thead>
-						<tr>
-							<td>Blog</td>
-							<td><a href="https://docs.soundctl.com">Documentation</a></td> 
-							<td><a href="mailto:admin@soundctl.com?Subject=Help%20me!"> Email Us </a></td>
-							<td> <span className="fa fa-github"></span> <a href="https://github.com/soundctl" target="_blank"> GitHub </a></td>
-						</tr>
-						<tr>
-							<td>Jobs</td>
-							<td>Features</td> 
-							<td>Call Us</td>
-							<td> <span className="fa fa-twitter"></span><a href="https://twitter.com/soundctl" target="_blank"> Twitter</a></td>
-						</tr>
-						<tr>
-							<td></td>
-							<td>Uptime & Status</td> 
-							<td></td> 
-							<td> <span className="fa fa-facebook"></span><a href="https://facebook.com/soundctl" target="_blank"> Facebook</a></td>
-						</tr>
-						<tr>
-							<td></td>
-							<td><Link to="/pricing"> Pricing </Link></td> 
-							<td></td> 
-						</tr>
-					</tbody>
-				</table>
+				<div className="uk-grid content-wrap">
+					<div className="uk-width-1-4">
+						<div className="text header"> Company </div>
+						<div className="text row"> Blog </div>
+						<div className="text row">Jobs </div>
+					</div>
+					<div className="uk-width-1-4">
+						<div className="text header"> Product </div>
+						<a className="text row" href="https://docs.soundctl.com"> Documentation </a>
+						<Link to="/pricing" className="text row"> Pricing </Link>
+						<div className="text row"> Uptime & Status </div>
+					</div>
+					<div className="uk-width-1-4">
+						<div className="text header"> Support </div>
+						<a className="text row" href="mailto:admin@soundctl.com?Subject=Help%20me!"> Email Us </a>
+						<div className="text row">Call Us </div>
+					</div>
+					<div className="uk-width-1-4">
+						<div className="text header"> Connect </div>
+						<div className="text row"><span className="fa fa-facebook"></span><a href="https://facebook.com/soundctl" target="_blank"> Facebook</a> </div>
+						<div className="text row"><span className="fa fa-twitter"></span><a href="https://twitter.com/soundctl" target="_blank"> Twitter</a></div>
+						<div className="text row"><span className="fa fa-github"></span> <a href="https://github.com/soundctl" target="_blank"> GitHub </a></div>
+					</div>
+				</div>
 			</div>
 		)
 	}
