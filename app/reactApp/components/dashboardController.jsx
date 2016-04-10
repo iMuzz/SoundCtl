25b326a9013e25e41143e301c3bdaeec2029354b
Faraaz Nishtar <faraaznishtar@gmail.com>
2016-04-09 19:50:02 -0700

    remove old radio component from dashboard

--- app/reactApp/components/dashboardController.jsx
+++ app/reactApp/components/dashboardController.jsx
@@ -1,35 +1,21 @@
 import React from                       'react'
-import {DashNav} from                   './dash-nav'
-import {StationManager} from            './stationManager'
-import {IntroUser} from                 './introductionFlow/introUser'
-import stationStoreInstance from        '../stores/stationStore'
-import introUserStoreInstance from      '../stores/IntroUserStore'
-import ProgressBar from                 'react-progress-bar-plus'
 import AppDispatcher from               '../dispatcher/AppDispatcher'
+
+// components
+import {DashNav} from                   './dash-nav'
 
 let UserActions = require('../actions/UserActions');
 
 export class DashboardController extends React.Component {
-
 	constructor(props) {
 		super(props);
-
 		this.state = {};
-		Object.assign(this.state, stationStoreInstance.getState());
-
-		this.handleStationStoreChange = this.handleStationStoreChange.bind(this);
 	}
 
 	componentDidMount(){
-		stationStoreInstance.addChangeListener(this.handleStationStoreChange);
 	}
 
 	componentWillUnmount(){
-		stationStoreInstance.removeChangeListener(this.handleStationStoreChange);
-	}
-
-	handleStationStoreChange(){
-		this.setState(stationStoreInstance.getState());
 	}
 
 	render() {
@@ -37,9 +23,7 @@ export class DashboardController extends React.Component {
 			<div>
 				<DashNav userProfile={this.props.profile}/>
 				<div className="dash-view">
-					<IntroUser station={this.state.station}/>
 				</div>
-				<ProgressBar />
 			</div>
 		);
 	}
