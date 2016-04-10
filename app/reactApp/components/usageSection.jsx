11ee367a867608f58d382602ac1d2af6a916943d
Faraaz Nishtar <faraaznishtar@gmail.com>
2016-04-10 02:51:44 -0700

    usage stats section mostly complete

--- app/reactApp/components/usageSection.jsx
+++ app/reactApp/components/usageSection.jsx
@@ -1,4 +1,4 @@
-import React from                       'react'
+import React from         'react'
 
 export class UsageSection extends React.Component {
   constructor(props) {
@@ -8,12 +8,12 @@ export class UsageSection extends React.Component {
   render() {
     return  ( 
       <div className='usage-section'>
-          <div>
+          <div className='container'>
               <h2> Monthly Usage </h2>
               <div className='usage-cards'>
-                <UsageCard></UsageCard>
-                <UsageCard></UsageCard>
-                <UsageCard></UsageCard>
+                <UsageCard uniqueId='circle-1' percentage={.75}></UsageCard>
+                <UsageCard uniqueId='circle-2' percentage={.33}></UsageCard> 
+                <UsageCard uniqueId='circle-3' percentage={.05}></UsageCard> 
               </div>
           </div>
       </div>
@@ -22,15 +22,50 @@ export class UsageSection extends React.Component {
 }
 
 class UsageCard extends React.Component {
-
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount(){
+    let id = '#' + this.props.uniqueId;
+    let that = this;
+     $(id).circleProgress({
+          value: this.props.percentage,
+          size: 200,
+          startAngle: 0,
+          lineCap: 'round',
+          thickness: 10,
+          emptyFill: '#FFE0D7',
+          fill: {
+              gradient: ['red', 'orange']
+          }
+      }).on('circle-animation-progress', function(event, progress) {
+          $(this).find('strong').html(parseInt((that.props.percentage * 100) * progress) + '<i>%</i>');
+      });
   }
 
   render() {
     return  ( 
       <div className='usage-card'>
-          Card
+          <div className='card-title'>
+            <h3> Transfer</h3>
+            <div className='dots'>
+              <div className='dot'></div>
+            </div>
+          </div>
+          <div className='card-dash'></div>
+          <div className='circle' id={this.props.uniqueId}>
+            <div className='circle-content'>
+              <strong className="inner-value">
+              </strong>
+              <span>Used</span>
+            </div>
+          </div>
+          <div className='usage-stats'>
+            <div className='usage-stat'> <span>36.5</span> <div className='stat-unit'> GB Used</div></div>
+            <div className='usage-divider'></div>
+            <div className='usage-stat'> <span>13.5</span> <div className='stat-unit'> GB Used</div></div>
+          </div>
       </div>  
     );
   }
