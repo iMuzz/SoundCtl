8bcb770cb8fc9555359b0a87910747b436049a80
Faraaz Nishtar <faraaznishtar@gmail.com>
2016-04-10 21:20:55 -0700

    Left nav icons added

--- app/reactApp/components/appController.jsx
+++ app/reactApp/components/appController.jsx
@@ -9,56 +9,56 @@ import activeComponent from                      'react-router-active-component'
 let UserActions = require('../actions/UserActions');
 
 export class AppController extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { 
-			profile: null
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = { 
+      profile: null
+    };
+  }
 
-	componentDidMount(){
-		this.props.lock.getProfile(this.props.idToken, (err, profile) => {
-			if(err) {
-				UserActions.logout();
-			}
-			this.setState({profile: profile});
-		});
-	}
+  componentDidMount(){
+    this.props.lock.getProfile(this.props.idToken, (err, profile) => {
+      if(err) {
+        UserActions.logout();
+      }
+      this.setState({profile: profile});
+    });
+  }
 
-	render() {
-		return  ( 
-			<div id="dashboard">
-				<SideNav />
-				<div className="dash-view-wrap">
-					<DashNav userProfile={this.state.profile}/>
-					{ React.cloneElement(this.props.children, {profile: this.state.profile}) }
-				</div>
-		</div>
-		);
-	}
+  render() {
+    return  ( 
+      <div id="dashboard">
+        <SideNav />
+        <div className="dash-view-wrap">
+          <DashNav userProfile={this.state.profile}/>
+          { React.cloneElement(this.props.children, {profile: this.state.profile}) }
+        </div>
+    </div>
+    );
+  }
 }
 
 class SideNav extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		let NavItem = activeComponent("li");
+  render() {
+    let NavItem = activeComponent("li");
 
-		return  ( 
-			<div className="left-nav">
-				<div className="company-logo-wrap">
-					<div className="company-logo">
-						<img className="wave" src="images/icons/waves.svg" alt=""/>
-						<div className="text"> SoundCTL </div>
-					</div>
-				</div>
-				<nav>
-					<NavItem className="tab" to="app/dashboard"> Dashboard </NavItem>
-					<NavItem className="tab" to="app/settings"> Settings </NavItem>
-				</nav>
-			</div>
-		);
-	}
+    return  ( 
+      <div className="left-nav">
+        <div className="company-logo-wrap">
+          <div className="company-logo">
+            <img className="wave" src="images/icons/waves.svg" alt=""/>
+            <div className="text"> SoundCTL </div>
+          </div>
+        </div>
+        <nav>
+          <NavItem className="tab" to="app/dashboard"> <div className='svg dash' ></div> Dashboard </NavItem>
+          <NavItem className="tab" to="app/settings"> <div className='svg settings' ></div> Settings </NavItem>
+        </nav>
+      </div>
+    );
+  }
 }
