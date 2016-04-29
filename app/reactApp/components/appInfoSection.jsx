52d35ed2209a55c4b7477012bbdfc49513a00778
Faraaz Nishtar <faraaznishtar@gmail.com>
2016-04-28 21:29:33 -0700

    copy to clipboard tooltip

--- app/reactApp/components/appInfoSection.jsx
+++ app/reactApp/components/appInfoSection.jsx
@@ -1,6 +1,7 @@
 import React from         'react';
 import AppDispatcher from '../dispatcher/AppDispatcher';
 import clipboard from 'clipboard';
+import ReactTooltip from 'react-tooltip'
 
 export class AppInfoSection extends React.Component {
   constructor(props) {
@@ -32,24 +33,25 @@ export class AppInfoSection extends React.Component {
                   <div className='label'> API Key</div>
                   <div className='content'>
                     <div className='input-container'>
-                      <input type="text" id='apiKey' readOnly value={this.props.apiKey}/>
+                      <input type="text" id='apiKey' value={this.props.apiKey}/>
                       <div className='cta' onClick={this.handleRegenerate}> Regenerate</div>
                     </div>
-                    <div className='copy' id='copy-key' data-clipboard-target="#apiKey" > </div>
+                    <div data-tip="Copy to clipboard" className='copy' id='copy-key' data-clipboard-target="#apiKey" > </div>
+                    <ReactTooltip place="bottom" type="dark" effect="solid"/>
                   </div>
                 </div>
                 <div className='row'>
                   <div className='label'> API Endpoint</div>
                   <div className='content'> 
                     <input type="text" id='apiEndpoint' readOnly value={'https://'+ this.props.instanceID+'.soundctl.io'}/>
-                    <div className='copy' id='copy-endpoint' data-clipboard-target='#apiEndpoint'> </div>
+                    <div data-tip="Copy to clipboard" className='copy' id='copy-endpoint' data-clipboard-target='#apiEndpoint'> </div>
                   </div>
                 </div>
                 <div className='row'>
                   <div className='label'> Instance ID</div>
                   <div className='content'> 
                     <input type="text" id='instanceID' readOnly value={this.props.instanceID}/> 
-                    <div className='copy' id='copy-instance' data-clipboard-target='#instanceID'> </div>
+                    <div data-tip="Copy to clipboard" className='copy' id='copy-instance' data-clipboard-target='#instanceID'> </div>
                   </div>
                 </div>
               </div>
